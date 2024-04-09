6be9e9f4747dd53e71297c345838569218e0b5de
zhengqi zhang <770166635@qq.com>
2024-04-09 09:12:35 +0800

    improve asstes

--- src/components/ComingSoon/index.js
+++ src/components/ComingSoon/index.js
@@ -2,7 +2,7 @@ const ComingSoon = () => {
   return (
     <div className="mx-auto text-center my-[9rem] max-md:my-[4.5rem]">
       <img
-        src="/images/comming_soon.png"
+        src="/images/comming_soon.svg"
         className="w-[320px] mb-2 mx-auto max-md:w-[24rem]"
       />
       <h1 className="font-semibold text-xl max-md:text-[18px]">Coming soon...</h1>
