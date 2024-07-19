be1e11f819657621e091e3c95bdaed2b27eb40c5
RH <93211672+PrintRH@users.noreply.github.com>
2024-07-19 13:36:15 +0800

    update banner

--- src/app/content/page.tsx
+++ src/app/content/page.tsx
@@ -1,23 +1,86 @@
 "use client";
 
-import List from "./List";
+import React from "react";
+import Image from "next/image";
+
 import { styled } from "@mui/material";
+import useCheckViewport from "@/hooks/useCheckViewport";
 
-const Header = styled("header")({
-  background: 'url("/images/banner.png")  center center / cover no-repeat',
-});
+import List from "./List";
 
-function ContentPage({}) {
+const ContentPage = () => {
+  const Header = styled("header")({
+    backgroundColor: "#FFF0DD",
+  });
+
+  const { isDesktop, isTabletLandscape, isTablet, isMobile } =
+    useCheckViewport();
+
+  let dynamicPaddingStyle = "";
+  if (isTabletLandscape) {
+    dynamicPaddingStyle = "pl-[96px]";
+  } else if (isTablet) {
+    dynamicPaddingStyle = "pl-[56px]";
+  } else if (isMobile) {
+    dynamicPaddingStyle = "pl-[42px]";
+  }
+
+  let dynamicImagePositioning = "";
+  if (isDesktop) {
+    dynamicImagePositioning = "right-56";
+  } else if (isTabletLandscape) {
+    dynamicImagePositioning = "right-48";
+  } else if (isTablet) {
+    dynamicImagePositioning = "right-32";
+  } else if (isMobile) {
+    dynamicImagePositioning = "right-16";
+  }
+
+  let dynamicSvgWidthStyle = 0;
+  if (isDesktop) {
+    dynamicSvgWidthStyle = 305;
+  } else if (isTabletLandscape) {
+    dynamicSvgWidthStyle = 250;
+  } else if (isTablet) {
+    dynamicSvgWidthStyle = 250;
+  } else if (isMobile) {
+    dynamicSvgWidthStyle = 150;
+  }
+
+  let dynamicSvgHeightStyle = 0;
+  if (isDesktop) {
+    dynamicSvgHeightStyle = 200;
+  } else if (isTabletLandscape) {
+    dynamicSvgHeightStyle = 175;
+  } else if (isTablet) {
+    dynamicSvgHeightStyle = 175;
+  } else if (isMobile) {
+    dynamicSvgHeightStyle = 100;
+  }
+
   return (
-    <div className="flex flex-col pb-[5rem] mt-[-6.5rem]">
-      <Header className="overflow-hidden relative flex items-center justify-center  w-full text-center h-[356px] max-md:max-w-full max-md:h-[18rem]">
-        <h1 className="relative self-center text-9xl font-semibold leading-[356px] text-stone-950 max-md:text-[3.6rem]">
-          Content
-        </h1>
+    <div className="mt-[-6.5rem] flex flex-col pb-2.5">
+      <Header className="justify-left relative flex h-[356px] w-full items-center overflow-hidden text-left max-md:h-[18rem] max-md:max-w-full">
+        <div
+          className={`relative mx-auto w-full ${isDesktop ? "max-w-[1536px] pl-[104px] text-9xl" : ""}`}
+        >
+          <h1
+            className={`${isMobile ? "text-7xl" : "text-8xl"} font-semibold leading-[356px] text-stone-950 max-md:text-[3.6rem] ${dynamicPaddingStyle} `}
+          >
+            Content
+            <Image
+              src="/images/content-banner.svg"
+              width={dynamicSvgWidthStyle}
+              height={dynamicSvgHeightStyle}
+              className={`absolute ${dynamicImagePositioning} top-1/2 mt-[3.0rem] -translate-y-1/2 transform`}
+              alt="Content"
+            />
+          </h1>
+        </div>
       </Header>
       <List />
     </div>
   );
-}
+};
 
 export default ContentPage;
