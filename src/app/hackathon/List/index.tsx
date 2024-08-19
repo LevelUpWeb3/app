832da14b9d39a84a985182c1f884c3bfd4cb93ab
RH <93211672+PrintRH@users.noreply.github.com>
2024-08-19 23:33:29 +0800

    add mobile view

--- src/app/hackathon/List/index.tsx
+++ src/app/hackathon/List/index.tsx
@@ -5,10 +5,8 @@ import { useMemo, useState, useEffect } from "react";
 import { Box } from "@mui/material";
 import { styled } from "@mui/system";
 import useScrollTrigger from "@mui/material/useScrollTrigger";
-
 import HackathonCard from "./HackathonCard";
-import DateSelect from "./DateSelect";
-import RegionSelect from "./RegionSelect";
+import AdaptableComponent from "./AdaptableSelect";
 import Data from "./../hackathonList.json";
 import {
   HACKATHON_DATE_LIST,
@@ -73,23 +71,14 @@ const List = () => {
   return (
     <Container>
       <Box sx={{ display: "flex", flexDirection: "column" }}>
-        <div className="mb-[2.4rem] mt-[4rem] flex text-[4rem] font-medium max-md:my-[2.4rem] max-md:text-[2.4rem]">
-          All Events
-          <div className="ml-auto flex gap-4">
-            <DateSelect
-              top={stickyTop}
-              sticky={isSticky}
-              value={dateParams.level}
-              onChange={handleChangeDate}
-            />
-            <RegionSelect
-              top={stickyTop}
-              sticky={isSticky}
-              value={regionParams.level}
-              onChange={handleChangeRegion}
-            />
-          </div>
-        </div>
+        <AdaptableComponent
+          stickyTop={stickyTop}
+          isSticky={isSticky}
+          dateParams={dateParams}
+          regionParams={regionParams}
+          handleChangeDate={handleChangeDate}
+          handleChangeRegion={handleChangeRegion}
+        />
         <CardBox>
           {filteredData.length > 0 ? (
             filteredData.map((item, index) => (
